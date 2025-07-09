ce84712c1ecd584a973b4c1aaccef875a854cd50
Faridaartawan <farida.artawan@gmail.com>
2025-07-09 13:32:55 +0700

    asdasdas

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// Toggle Menu on Small Screens
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
